fc381fd631d8a9da987babb04f219d511c4cda76
imkewlhuh <rafael.lopez0827@gmail.com>
2023-02-21 19:53:49 -0500

    commit to self

--- auth/index.js
+++ auth/index.js
@@ -1,1 +1,17 @@
 //This is where we will add our auth strategies
+import { Strategy, ExtractJwt } from "passport-jwt";
+
+export default function setupJWTStrategy(passport) {
+    passport.use(
+        new Strategy({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: "thisIsASecret"
+        }, function (payload, done) {
+            try {
+                return done(null, { username: payload.username, id: payload.id });
+            } catch (e) {
+                return done(e, null);
+            }
+        })
+    )
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,103 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import prisma from "../db/index.js";
+import argon2 from "argon2";
 
 const router = express.Router();
 
-router.post("/login", (request, response) => {
+router.post("/login", async (request, response) => {
   //Handle login
+
+  try {
+    const foundUser = await prisma.user.findFirst({
+      where: {
+        username: request.body.username
+      }
+    });
+
+    if (foundUser) {
+      try {
+        const verifyPassword = await argon2.verify(foundUser.password, request.body.password);
+
+        if (verifyPassword) {
+          const token = jwt.sign({ username: foundUser.username, id: foundUser.id },
+            "thisIsASecretKey");
+
+          response.status(200).json({
+            success: true,
+            token
+          });
+        } else {
+          response.status(401).json({
+            success: false,
+            message: "Incorrect username or password"
+          });
+        }
+      } catch (e) {
+        response.status(500).json({
+          success: false,
+          message: "Something went wrong"
+        });
+      }
+    }
+  } catch (e) {
+    response.status(500).json({
+      success: false,
+      message: "Something went wrong"
+    });
+  }
 });
 
-router.post("/signup", (request, response) => {
+router.post("/signup", async (request, response) => {
   //handle signup
+
+  try {
+    const foundUser = await prisma.user.findFirst({
+      where: {
+        username: request.body.username
+      }
+    });
+
+    if (foundUser) {
+      response.status(401).json({
+        success: false,
+        message: "User already exists"
+      });
+    } else {
+      try {
+        const hashedPassword = await argon2.hash(request.body.password);
+
+        const newUser = await prisma.user.create({
+          data: {
+            username: request.body.username,
+            password: hashedPassword
+          }
+        });
+
+        if (newUser) {
+          response.status(201).json({
+            success: true,
+            message: "User successfully created"
+          });
+        } else {
+          response.status(500).json({
+            success: false,
+            message: "User was not created. Something happened."
+          });
+        }
+      } catch (err) {
+        response.status(500).json({
+          success: false,
+          message: "User was not created. Something happened."
+        });
+      }
+    }
+  } catch (e) {
+    response.status(500).json({
+      success: false,
+      message: "Something went wrong"
+    });
+  }
 });
 
 export default router;
